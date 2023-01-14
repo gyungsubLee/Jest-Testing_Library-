aeb92b9862fc903e2cc57df293fbe3892db09fe0
gyungsubLee <ditjq2323@gmail.com>
2023-01-14 14:34:31 +0900

    1.2 - Add test code / Color is to be blue and change text 'Change to red'  When clicked
    - test code - click event
    - fail test code -> don't make the Code Changing the Button DOM  to be blue

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,8 +9,14 @@ test('button has correct initial color', () => {
 
     // expect the background color to be red
     expect(colorButton).toHaveStyle({ backgroundColor: 'red'}); 
+
+    // click button
+    fireEvent.click(colorButton);
+
+    // expect the background color to blue
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+    // expect the button text to be 'Change to red'
+    expect(colorButton).toHaveTextContent('Change to red');
 });
 
-test('button turns blue when clicked', ()=>{
-
-});

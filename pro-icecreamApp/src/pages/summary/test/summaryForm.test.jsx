436579ac240fe9ce1a5f2ec81e34e87597d57b8a
gyungsubLee <ditjq2323@gmail.com>
2023-02-18 21:24:44 +0900

    3.2 - refactor: fireEvnet -> useEvent - update useEvnet v14 - change 'fireEvent' to 'useEvent' - Notes/useEvent 정리 중
    [23.02.18]

--- pro-icecreamApp/src/pages/summary/test/summaryForm.test.jsx
+++ pro-icecreamApp/src/pages/summary/test/summaryForm.test.jsx
@@ -1,26 +1,33 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import SummaryForm from "../summaryForm";
+import {render, fireEvent, screen} from '@testing-library/react'
+import  userEvent  from '@testing-library/user-event';
+import SummaryForm from '../summaryForm';
 
-test("Initial conditions", () => {
+test('init codition', ()=>{
   render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /terms and conditions/i,
-  });
+
+  const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
   expect(checkbox).not.toBeChecked();
 
-  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
   expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
+
+
+test('Checkbox enables button on first click and disabbles on second click', async ()=>{
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /terms and conditions/i,
-  });
-  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
-  fireEvent.click(checkbox);
+
+  const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
+
+  await user.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+
+  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
-});
+})

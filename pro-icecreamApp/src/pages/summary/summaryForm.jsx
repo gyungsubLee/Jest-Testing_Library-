849aabe66903cb5c4e2f4ce8a1e944221b983b14
gyungsubLee <ditjq2323@gmail.com>
2023-02-24 20:57:19 +0900

    3.3 - checkbox에 hover/unhover  시 팝업창 추가/삭제
    -react bootstrap(popover, overlayTrigger)을 사용하여 팝업창 구현
    -test code: hover/unhover 이벤트 테스트
    
    [23.02.24]

--- pro-icecreamApp/src/pages/summary/summaryForm.jsx
+++ pro-icecreamApp/src/pages/summary/summaryForm.jsx
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
-export default function SummaryForm() {
+export default function SummaryForm({ setOrderPhase }) {
   const [tcChecked, setTcChecked] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setOrderPhase("completed");
+  };
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
 
   const checkboxLabel = (
     <span>
       I agree to
-      <span style={{ color: "blue" }}> Terms and Conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
--- pro-icecreamApp/src/pages/summary/test/summaryForm.test.jsx
+++ pro-icecreamApp/src/pages/summary/test/summaryForm.test.jsx
@@ -1,33 +1,57 @@
-import {render, fireEvent, screen} from '@testing-library/react'
-import  userEvent  from '@testing-library/user-event';
-import SummaryForm from '../summaryForm';
+import { render, fireEvent, screen } from "@testing-library/react";
+import SummaryForm from "../summaryForm";
+import userEvent from "@testing-library/user-event";
 
-test('init codition', ()=>{
+test("init condittion", () => {
   render(<SummaryForm />);
 
-  const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
   expect(checkbox).not.toBeChecked();
 
-  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
-  expect(confirmButton).toBeDisabled();
+  const confrimButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confrimButton).toBeDisabled();
 });
 
-
-
-test('Checkbox enables button on first click and disabbles on second click', async ()=>{
+test("Checkbox enables button on first click and disables on second click", async () => {
   const user = userEvent.setup();
 
   render(<SummaryForm />);
 
-  const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confrimButton = screen.getByRole("button", { name: /confirm order/i });
 
+  // first click
   await user.click(checkbox);
+  expect(confrimButton).toBeEnabled();
 
-  expect(checkbox).toBeChecked();
-
-  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
-  expect(confirmButton).toBeEnabled();
-
+  // second click
   await user.click(checkbox);
-  expect(confirmButton).toBeDisabled();
-})
+  expect(checkbox).not.toBeChecked();
+  expect(confrimButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await userEvent.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});

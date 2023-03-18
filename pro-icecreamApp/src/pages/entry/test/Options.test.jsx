e551ac38ff3937c0780cb7acff7ea06c7ff1bcdb
gyungsubLee <ditjq2323@gmail.com>
2023-03-18 21:48:58 +0900

    4.2 - Code Quiz: Code, test Topping option images from sever
    * Add mock topping data
    * Update Options.jsx: ItemComponent "null" ->
    "ToppingOption"
    * Add test code: Topping Images (success)
    
    [23.03.18]

--- pro-icecreamApp/src/pages/entry/test/Options.test.jsx
+++ pro-icecreamApp/src/pages/entry/test/Options.test.jsx
@@ -4,12 +4,31 @@ import Options from "../Options";
 test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
-  // find images
+  // find scoop images
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
-  // confirm alt text of images
+  // confirm alt text of scoop images
   //@ts-ignore
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("display image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find topping images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of topping Images
+  //@ts-ignore
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });

15f35faeeb33819729f59c202ca4328976efa0b9
gyungsubLee <ditjq2323@gmail.com>
2023-03-24 20:38:10 +0900

    4.4 - Fix test code: 서버 응답 test - waitFor ->  두개 서버(scoops, toppings) 호출 처리 후 expect() 실행 [race condition]
    [23.03.24]

--- pro-icecreamApp/src/pages/common/AlertBanner.jsx
+++ pro-icecreamApp/src/pages/common/AlertBanner.jsx
@@ -1,11 +1,12 @@
-import { Alert } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 
 
 export default function AlertBanner ({header, message, variant }) {
     const AlertHeader = header || "Error"
     const AlertMassage = message || "An unexpected error occurred. Please try again later.";
     const AlertVariant = variant || "danger";
-    return <Alert variant={AlertVariant}>
+
+    return <Alert variant={AlertVariant} style={{ backgroundColor: 'red', padding:'10px', margin:'10px'}}>
         <Alert.Heading>{AlertHeader}</Alert.Heading>
         <p>{AlertMassage}</p>
     </Alert>
--- pro-icecreamApp/src/pages/entry/tests/OrderEntry.test.jsx
+++ pro-icecreamApp/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,29 +1,30 @@
-import {render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
 
-
 // 1. mock sever data  Reset
-test('handles error for scoops and toppings routes',async ()=>{
-    server.resetHandlers(
-        rest.get("http://localhost3030/scoops", (req, res, ctx)=>{
-            return res(ctx.status(500));
-        }),
-        rest.get("http://localhost3030/toppings", (req, res, ctx)=>{
-            return res(ctx.status(500));
-        })
-    )
+test("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost3030/scoops", (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+    rest.get("http://localhost3030/toppings", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
 
-    // 2. 테스트하려는 DOM renderimg
-    render(<OrderEntry/>);
+  // 2. 테스트하려는 DOM renderimg
+  render(<OrderEntry />);
 
-    //3.  해당 DOM 정상적으로 생성되는지 test
-    const alerts = await screen.findAllByRole("alert", {
-       name: "An unexpected error occurred. Please try again later." 
-    });
+  //3.  해당 DOM 정상적으로 생성되는지 test
 
-    // 가져온 DOM 설계와 일치하는지 test
+  // 가져온 DOM 설계와 일치하는지 test
+
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
+  });
 });
 
+test("mock test", () => {});

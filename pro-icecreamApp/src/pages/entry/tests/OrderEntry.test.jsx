ef5643dd68f3f913f8a3d8c93e590c6eb651bb80
gyungsubLee <ditjq2323@gmail.com>
2023-03-28 19:30:10 +0900

    5.0 - scoops/toppings 개수 및 가격 테스트: test code
    * Add [totalUpdate.test.jsx]
     1. 초기 가격: "0.00" test (getByText, toHaveTextContent)
     2. scoop/vanilla 1개, 가격 test (findByRole, userEvent[clear, type], toHaveTextContent)
     3. scoop/chocolate 2개, 가격 test (위와 동일)
    
    [23.03.28]

--- pro-icecreamApp/src/pages/entry/tests/OrderEntry.test.jsx
+++ pro-icecreamApp/src/pages/entry/tests/OrderEntry.test.jsx
@@ -6,10 +6,10 @@ import { server } from "../../../mocks/server";
 // 1. mock sever data  Reset
 test("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
-    rest.get("http://localhost3030/scoops", (req, res, ctx) => {
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
       return res(ctx.status(500));
     }),
-    rest.get("http://localhost3030/toppings", (req, res, ctx) => {
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
       return res(ctx.status(500));
     })
   );

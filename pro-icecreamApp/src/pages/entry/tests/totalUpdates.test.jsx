d1da1991ec7182b9c80ff2b6fc6cd94f54c35aef
gyungsubLee <ditjq2323@gmail.com>
2023-04-19 19:08:15 +0900

    5.3 Code Quiz: test code of topping options - Add test code of topping options - userEvent를 사용한  toppingCheckbox, total topping price test
    [23.04.19]

--- pro-icecreamApp/src/pages/entry/tests/totalUpdates.test.jsx
+++ pro-icecreamApp/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,32 +1,74 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import OrderEntry from "../OrderEntry";
 
-test("Update scoop subtotal when scoops change", async () => {
+// Test Options Scoops
+test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-
   render(<Options optionType="scoops" />);
 
-  // make sure total starts out at $0.00
+  // make sure total starts out $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
   expect(scoopsSubtotal).toHaveTextContent("0.00");
 
-  // update vanilla scoops to 1, and checksubtotal
-  const vanillaInput = await screen.findByRole("spinButton", {
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole("spinbutton", {
     name: "Vanilla",
   });
-
   await user.clear(vanillaInput);
   await user.type(vanillaInput, "1");
   expect(scoopsSubtotal).toHaveTextContent("2.00");
 
-  // update chocolate scoops to 2, and check subtotal
-
-  const chocolateInput = await findByRole("spinButton", {
+  // update chocolate scoops to 2 and check subtotal
+  const chocolateInput = await screen.findByRole("spinbutton", {
     name: "Chocolate",
   });
-
   await user.clear(chocolateInput);
   await user.type(chocolateInput, "2");
-  expect(scoopsSubtotal, tothaveTextContent("6.00"));
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+// Test Options Toppings
+
+test("update topping subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  // make sure Toppings Total starts out $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  const cherriesCheckBox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+
+  // make sure TopingCheckbox starts unchecked.
+  expect(cherriesCheckBox).not.toBeChecked();
+
+  // when clicked checkbox,
+  await user.click(cherriesCheckBox);
+  expect(cherriesCheckBox).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // when user clicked CherrisCheckbox, CherriesCheckbox is unchecked and update total price
+  await user.click(cherriesCheckBox);
+  expect(cherriesCheckBox).toBeChecked();
+  expect(cherriesCheckBox).toHaveTextContent("0.00");
+
+  await user.click(cherriesCheckBox);
+
+  const hotFugeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot Fudge ",
+  });
+
+  // make sure TopingCheckbox starts unchecked.
+  expect(hotFugCheckbox).not.toBeChecked();
+
+  await user.click(hotFugeCheckbox);
+  expect(hotFugeCheckbox).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
 });
